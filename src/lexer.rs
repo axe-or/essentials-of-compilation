05613698838f9051f46907e8c90a5a57cd263512
marcs feh <marcos.fehlauer@digiaccess.com.br>
2025-12-03 14:57:33 -0300

    yeah

---
+++ src/lexer.rs
@@ -0,0 +1,291 @@
+use crate::Error;
+
+pub struct Lexer {
+     source: Vec<char>,
+    pub(crate)current: usize,
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub enum Token {
+    Identifier(String),
+    String(String),
+    Integer(i64),
+    Real(f64),
+
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Modulo,
+
+    ShiftRight,
+    ShiftLeft,
+    Tilde,
+    And,
+    Or,
+
+    Equal,
+    NotEqual,
+    Gt,
+    GtEq,
+    Lt,
+    LtEq,
+
+    Assign,
+    Dot,
+    Semicolon,
+    Colon,
+    Comma,
+
+    Fn,
+    If,
+    Else,
+    Let,
+
+    EndOfFile,
+}
+
+fn is_part_of_identifier(c: char) -> bool {
+    match c {
+        'a'..='z' | 'A'..='Z' | '0'..'9' | '_' => true,
+        _ => false,
+    }
+}
+
+impl Lexer {
+    pub fn new(source: &str) -> Lexer {
+        Lexer {
+            source: source.chars().collect(),
+            current: 0,
+        }
+    }
+
+    pub fn make_lexeme(&self, start: usize, end: usize) -> String {
+        (&self.source[start..end]).iter().collect()
+    }
+
+    pub fn advance(&mut self) -> Option<char> {
+        if self.current >= self.source.len(){
+            return None;
+        }
+        self.current += 1;
+        return Some(self.source[self.current - 1]);
+    }
+
+    pub fn peek(&self) -> Option<char> {
+        if self.current >= self.source.len(){
+            return None;
+        }
+        return Some(self.source[self.current]);
+    }
+
+    fn skip_whitespace(&mut self){
+        loop {
+            let c = match self.advance() {
+                Some(c) => c,
+                None => break,
+            };
+
+            if !c.is_whitespace(){
+                self.current -= 1;
+                break;
+            }
+        }
+    }
+
+    fn scan_identifier(&mut self) -> Token {
+        let start = self.current;
+
+        loop {
+            let c = match self.advance(){
+                Some(c) => c,
+                None => break,
+            };
+
+            if !is_part_of_identifier(c){
+                self.current -= 1;
+                break;
+            }
+        }
+
+        assert!(self.current > start, "Invalid identifier length");
+
+        let lexeme: String = self.make_lexeme(start, self.current);
+        if let Some(tok) = as_keyword(&lexeme){
+            return tok;
+        }
+
+        return Token::Identifier(lexeme);
+    }
+
+    fn scan_string(&mut self) -> Result<Token, Error> {
+        assert!(self.advance() == Some('"'), "Invalid lexer position");
+
+        let mut buf = String::new();
+
+        loop {
+            let c = match self.advance() {
+                Some(c) => c,
+                None => return Err(Error::UnterminatedString),
+            };
+
+            if c == '\\' {
+                let next_char = match self.advance() {
+                    Some(c) => c,
+                    None => return Err(Error::UnterminatedString),
+                };
+
+                let escaped = match escape_sequence(next_char) {
+                    Some(c) => c,
+                    None => return Err(Error::InvalidEscapeSequence),
+                };
+
+                buf.push(escaped);
+            }
+            else if c == '\n' || c == '\r' {
+                return Err(Error::InvalidMultiLineString);
+            }
+            else if c == '"' {
+                break;
+            }
+            else {
+                buf.push(c);
+            }
+
+        }
+
+        let tk = Token::String(buf);
+        return Ok(tk);
+    }
+
+    fn scan_decimal_integer(&mut self) -> Result<Token, Error>{
+        let start = self.current;
+
+        loop {
+            let c = match self.advance(){
+                Some(c) => c,
+                None => break,
+            };
+
+            if !c.is_numeric() && c != '_' {
+                self.current -= 1;
+                break;
+            }
+        }
+
+        let lexeme = self.make_lexeme(start, self.current);
+        println!("'{}'", lexeme);
+        let num = lexeme.parse::<i64>().expect("Invalid integer");
+
+        return Ok(Token::Integer(num));
+    }
+
+    fn match_advance(&mut self, target: char) -> bool {
+        if let Some(c) = self.peek() {
+            if c == target {
+                self.current += 1;
+            }
+            return c == target;
+        }
+        return false;
+    }
+
+    pub fn next(&mut self) -> Result<Token, Error> {
+        use Token as T;
+
+        self.skip_whitespace();
+
+        let c = match self.peek() {
+            Some(c) => c,
+            None => return Ok(Token::EndOfFile),
+        };
+
+        if c.is_numeric(){
+            return self.scan_decimal_integer();
+        }
+
+        if is_part_of_identifier(c){
+            return Ok(self.scan_identifier());
+        }
+
+        if c == '"' {
+            return self.scan_string();
+        }
+
+        _ = self.advance();
+
+        let tk = match c {
+            '.' => Ok(T::Dot),
+            ':' => Ok(T::Colon),
+            ',' => Ok(T::Comma),
+            ';' => Ok(T::Semicolon),
+
+            '+' => Ok(T::Plus),
+            '-' => Ok(T::Minus),
+            '*' => Ok(T::Minus),
+            '/' => Ok(T::Slash),
+            '%' => Ok(T::Modulo),
+            '~' => Ok(T::Tilde),
+            '|' => Ok(T::Or),
+            '&' => Ok(T::And),
+            '=' => if self.match_advance('='){
+                Ok(T::Equal)
+            } else {
+                Ok(T::Assign)
+            }
+
+            '>' => if self.match_advance('='){
+                Ok(T::GtEq)
+            } else if self.match_advance('>') {
+                Ok(T::ShiftRight)
+            } else {
+                Ok(T::Gt)
+            },
+
+            '<' => if self.match_advance('='){
+                Ok(T::LtEq)
+            } else if self.match_advance('<') {
+                Ok(T::ShiftLeft)
+            } else {
+                Ok(T::Lt)
+            },
+
+            _ => Err(Error::UnknownCodepoint),
+        };
+
+        return tk;
+    }
+}
+
+fn as_keyword(s: &str) -> Option<Token> {
+    use Token as T;
+
+    static KEYWORDS: [(&'static str, Token); 4] = [
+        ("if", T::If),
+        ("else", T::Else),
+        ("let", T::Let),
+        ("fn", T::Fn),
+    ];
+
+    for (key, token) in &KEYWORDS {
+        if s == *key {
+            return Some(token.clone());
+        }
+    }
+
+    return None;
+}
+
+fn escape_sequence(c: char) -> Option<char> {
+    match c {
+        'r' => Some('\r'),
+        'n' => Some('\n'),
+        't' => Some('\t'),
+        'e' => Some('\x1B'),
+        '"' => Some('"'),
+        '\'' => Some('\''),
+        '\\' => Some('\\'),
+        _ => None,
+    }
+}
+
